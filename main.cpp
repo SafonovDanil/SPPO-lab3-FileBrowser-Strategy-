c0105e779497e29dcc5c0619df33bfe6a32e82e4
Danil Safonov <samiyleviymail@gmai.com>
2021-05-30 14:34:58 +0700

    Реализована функция подсчета размера папки

--- main.cpp
+++ main.cpp
@@ -7,23 +7,42 @@
 #include <QFileInfoList>
 #include <QApplication>
 #include <QFileSystemModel>
+#include <QDebug>
+
+
 class IStrategy
 {
 public:
-    virtual void CalculationMethod(QString) = 0;
+    virtual void CalculationMethod(const QString&) = 0;
+    double fullDirectorySize(QString path)
+    {
+        double direcctorySize = 0;
+        QDir directory(path);
+        QFileInfoList entryList = directory.entryInfoList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot);
+        for(auto &curItem : entryList)
+        {
+            if(curItem.isDir())
+                direcctorySize += fullDirectorySize(path + '/' +curItem.fileName());
+            else
+                direcctorySize += curItem.size();
+        }
+        return direcctorySize;
+    }
+
 };
 
 class ByFolder_CalculationStrategy:public IStrategy
 {
 public:
-    void CalculationMethod(QString path){
-}
+    void CalculationMethod(const QString& path)
+    {
+    }
 };
 
 class ByFileType_CalculationStrategy:public IStrategy
 {
 public:
-    void CalculationMethod(QString path){
+    void CalculationMethod(const QString path){
 
     };
 };
@@ -38,12 +57,12 @@ void PrintFolderInfo(QString path, IStrategy* strat)
 int main(int argc, char *argv[])
 {
     ByFolder_CalculationStrategy * strat1 = new ByFolder_CalculationStrategy;
-    ByFileType_CalculationStrategy * strat2 = new ByFileType_CalculationStrategy;
-    QString path("path");
+    //ByFileType_CalculationStrategy * strat2 = new ByFileType_CalculationStrategy;
+    QString path("C:/Users/samiy/Documents/FIFA 21");
     std::cout << std::endl << "strat1:" << std::endl;
     PrintFolderInfo(path, strat1);
-    std::cout << std::endl << "strat2:" << std::endl;
-    PrintFolderInfo(path, strat2);
+    //std::cout << std::endl << "strat2:" << std::endl;
+    //PrintFolderInfo(path, strat2);
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
