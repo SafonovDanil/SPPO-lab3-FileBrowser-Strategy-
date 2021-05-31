6a5c6337bdacd522c0722f44a678257de991a2c7
Danil Safonov <samiyleviymail@gmai.com>
2021-05-31 13:46:38 +0700

    Реализована стратегия подсчета размера директории по типам
    + небольшие исправления

--- main.cpp
+++ main.cpp
@@ -16,19 +16,18 @@ public:
     virtual void CalculationMethod(const QString&) = 0;
     double fullDirectorySize(QString path)
     {
-        double direcctorySize = 0;
+        double dirSize = 0;
         QDir directory(path);
         QFileInfoList entryList = directory.entryInfoList(QDir::Dirs | QDir::Files | QDir::NoDotAndDotDot);
         for(auto &curItem : entryList)
         {
             if(curItem.isDir())
-                direcctorySize += fullDirectorySize(path + '/' +curItem.fileName());
+                dirSize += fullDirectorySize(path + '/' +curItem.fileName());
             else
-                direcctorySize += curItem.size();
+                dirSize += curItem.size();
         }
-        return direcctorySize;
+        return dirSize;
     }
-
 };
 
 class ByFolder_CalculationStrategy:public IStrategy
@@ -39,24 +38,81 @@ public:
     QDir dir(path);
     QFileInfoList fileInfoList = dir.entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot);
     QList<std::pair<QString,double>> filesSizeList;
-    if(!path.isEmpty())
+        if(!dir.exists())
+           throw QString("directory does not exist");
+        QFileInfoList filesInfoList = dir.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot);
+        double dirSize = this->fullDirectorySize(path);
+        for (QFileInfo &curItem : filesInfoList)
         {
-            if(!dir.exists())
-                throw QString("Directory does not exist");
-            QFileInfoList filesInfoList = dir.entryInfoList(QDir::AllEntries | QDir::NoDotAndDotDot);
-            double dirSize = this->fullDirectorySize(path);
+            double itemSize;
+            if(curItem.isDir())
+                itemSize = this->fullDirectorySize(path + '/' +curItem.fileName())/dirSize;
+            else
+                itemSize = (double)curItem.size()/dirSize;
+            filesSizeList.push_back(std::pair<QString,double> (curItem.fileName(),itemSize));
+        }
+
+    for(auto& curFile : filesSizeList)
+    {
+        if (curFile.second >= 0.01)
+            std::cout << curFile.first.toStdString() << "  " <<  (int)(curFile.second*100)/100.  << " %" << std::endl;
+        else if(curFile.second)
+            std::cout << curFile.first.toStdString() << "  " <<  "<0.01 %"  << std::endl;
+        else
+            std::cout << curFile.first.toStdString() << "  " <<  "0 %"  << std::endl;
+    }
+    if(filesSizeList.isEmpty())
+        std::cout << "empty directory\n";
+    }
+};
+
+class ByFileType_CalculationStrategy:public IStrategy
+{
+public:
+    void CalculationMethod(const QString& path)
+    {
+        QDir dir(path);
+        //QFileInfoList filesInfoList = dir.entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot);
+        QList<std::pair<QString,double>> typesSizeList;
+        if(!dir.exists())
+            throw QString("directory does not exist");
+        QFileInfoList filesInfoList = dir.entryInfoList(QDir::Files);
+        QFileInfoList dirList = dir.entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot);
+        QFileInfoList::iterator iterFileInfo = dirList.begin();;
+        while(iterFileInfo != dirList.end())
+        {
+            filesInfoList += QDir(iterFileInfo->absoluteFilePath()).entryInfoList(QDir::Files);
+            dirList += QDir(iterFileInfo->absoluteFilePath()).entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot);
+            iterFileInfo++;
+        }
+//            for(auto &curDir : dirList)
+//            {
+//                filesInfoList += QDir(curDir.absoluteFilePath()).entryInfoList(QDir::Files);
+//                dirList += QDir(curDir.absoluteFilePath()).entryInfoList(QDir::Dirs | QDir::NoDotAndDotDot);
+//            }
+        double dirSize = this->fullDirectorySize(path);
+        //QFileInfoList::iterator iterFileInfo;
+        QList <QString> typesList;
+        iterFileInfo = filesInfoList.begin();
+        while(iterFileInfo != filesInfoList.end())
+        {
+            if(!typesList.contains(iterFileInfo->suffix()))
+                typesList.push_back(iterFileInfo->suffix());
+            iterFileInfo++;
+        }
+        QString curSuffix;
+        for (QString &type : typesList)
+        {
+            curSuffix = type;
+            typesSizeList.append(std::pair<QString,double>(curSuffix,0));
             for (QFileInfo &curItem : filesInfoList)
             {
-                double itemSize;
-                if(curItem.isDir())
-                    itemSize = this->fullDirectorySize(path + '/' +curItem.fileName())/dirSize;
-                else
-                    itemSize = (double)curItem.size()/dirSize;
-                filesSizeList.push_back(std::pair<QString,double> (curItem.fileName(),itemSize));
+                if(curItem.suffix() == curSuffix)
+                typesSizeList.last().second += curItem.size();
             }
+            typesSizeList.last().second /= dirSize;
         }
-        //std::cout << curDir.fileName().toStdString() << ":\n";
-        for(auto& curFile : filesSizeList)
+        for(auto& curFile : typesSizeList)
         {
             if (curFile.second >= 0.01)
                 std::cout << curFile.first.toStdString() << "  " <<  (int)(curFile.second*100)/100.  << " %" << std::endl;
@@ -65,25 +121,9 @@ public:
             else
                 std::cout << curFile.first.toStdString() << "  " <<  "0 %"  << std::endl;
         }
-        if(filesSizeList.isEmpty())
-            std::cout << "empty directory\n";
-
-
-
-
-
-
-
-
+        if(typesSizeList.isEmpty())
+        std::cout << "empty directory" << std::endl;
     }
-};
-
-class ByFileType_CalculationStrategy:public IStrategy
-{
-public:
-    void CalculationMethod(const QString path){
-
-    };
 };
 
 void PrintFolderInfo(QString path, IStrategy* strat)
@@ -96,12 +136,14 @@ void PrintFolderInfo(QString path, IStrategy* strat)
 int main(int argc, char *argv[])
 {
     ByFolder_CalculationStrategy * strat1 = new ByFolder_CalculationStrategy;
-    //ByFileType_CalculationStrategy * strat2 = new ByFileType_CalculationStrategy;
-    QString path("C:/Users/samiy/Documents/FIFA 21");
+    ByFileType_CalculationStrategy * strat2 = new ByFileType_CalculationStrategy;
+
+    QString path("C:/Users/samiy/Documents/lab_2");
+
     std::cout << std::endl << "strat1:" << std::endl;
     PrintFolderInfo(path, strat1);
-    //std::cout << std::endl << "strat2:" << std::endl;
-    //PrintFolderInfo(path, strat2);
+    std::cout << std::endl << "strat2:" << std::endl;
+    PrintFolderInfo(path, strat2);
     //QApplication a(argc, argv);
     //MainWindow w;
     //w.show();
